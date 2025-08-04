2c8ee216e567b64022d3c3b33d69215d6027bfe5
Dekoder-py <dekod3r.py@gmail.com>
2025-08-05 10:40:53 +1200

    add text field

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Markdown Notes")
 root.geometry("800x600")
+
+# text entry space
+text_field = tk.Text(root, wrap="word", font=("", 16))
+
+text_field.pack(expand=True, fill="both")
+text_field.focus()
 root.mainloop()
